87ac62985db6faf65ff420ccd2ae8738d43c7af8
Jamile Farias <fariasjamile1234@gmail.com>
2023-03-14 12:31:05 -0300

    Projeto Java world

---
+++ hellowolrd/src/hellowolrd/HelloWorld.java
@@ -0,0 +1,14 @@
+package hellowolrd;
+
+public class HelloWorld {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method 
+int teste = 1;
+   System.out.println("Hello World");
+   System.out.println("\nturma 62");
+   System.out.println("pois bem\n");
+   System.out.println("Variavel teste" + teste);
+	}
+
+}
