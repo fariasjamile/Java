7fd12413ebc165035f19a22abffe956c24e74b4f
Jamile Farias <fariasjamile1234@gmail.com>
2023-03-24 22:15:25 -0300

    Aula_07

--- aula_07/src/aula_07/TestaCliente.java
+++ aula_07/src/aula_07/TestaCliente.java
@@ -1,5 +1,16 @@
 package aula_07;
 
 public class TestaCliente {
+	
+	 public static void main(String[] args) {
+
+		  Exer01_Cliente tc = new Exer01_Cliente("Jamille Farias" , 18, "Feminino" , "São Paulo" , "(11) 98856-0234");
+	        tc.visualizar();
+	        
+	        System.out.println("\n");
+	        
+	        Exer01_Cliente tc1 = new Exer01_Cliente("Heimy Dias" , 19, "Feminino" , "São Paulo" , "(11) 92251-7349");
+	        tc1.visualizar();
+}
 
 }
